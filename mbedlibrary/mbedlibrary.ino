fd635b04bb52c773d94b90bf16370f5f2e2351c7
Matt Parsons <h5n1xp@gmail.com>
2014-01-31 01:18:37 +0000

    IO Pin mappings
    Mbed pins start at 5 not 0…

--- mbedlibrary/mbedlibrary.ino
+++ mbedlibrary/mbedlibrary.ino
@@ -10,7 +10,7 @@
 #define PullDown  0; // pin mode: pulldown
 #define PullUp    1; // pin mode: pullup
 
-enum IOPins {p0=1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30};
+enum IOPins {p5=0,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30};
 
 //**************** DigitalOut
 class DigitalOut {
